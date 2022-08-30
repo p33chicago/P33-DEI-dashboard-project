77b674b3e8e5e70fbad39422eb88d459312263f8
John Syrinek <john.syrinek@gmail.com>
2022-08-30 22:34:52 +0000

    theme for compiled website

--- p33py/create_figure_json.py
+++ p33py/create_figure_json.py
@@ -6,6 +6,7 @@ import os
 data_dir=os.path.normpath(f'{os.path.dirname(__file__)}/../data/')
 json_path = f'{data_dir}/figure.json'
 
+set_default_theme()
 fig = apcs_score5.figure()
 # display(fig)
 pio.write_json(fig, json_path)
