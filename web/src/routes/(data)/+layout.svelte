942247052007dfa2b6f6623768ef16bf3800478f
John Syrinek <john.syrinek@gmail.com>
2022-09-08 21:30:36 +0000

    rough subnav

--- web/src/routes/(data)/+layout.svelte
+++ web/src/routes/(data)/+layout.svelte
@@ -1,5 +1,12 @@
-&lt;- back
-
-[Access] [Proficiency] [Excellence]
+<div class="flex m-2">
+    <div class="flex-none w-96">
+        <a href="/">&larr; Back</a>
+    </div>
+    <div class="flex-auto space-x-2">
+        <a class="bg-white border-2 rounded-full py-2 px-6 uppercase" href="access">Access</a>
+        <a class="bg-white border-2 rounded-full py-2 px-6 uppercase" href="proficiency">Proficiency</a>
+        <a class="bg-white border-2 rounded-full py-2 px-6 uppercase" href="excellence">Excellence</a>
+    </div>
+</div>
 
 <slot />
