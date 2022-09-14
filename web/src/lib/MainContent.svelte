eeb4316b1a7e3cad6333d5f8efa08df479a5a8f2
John Syrinek <john.syrinek@gmail.com>
2022-09-14 17:50:04 -0500

    grid changes

---
+++ web/src/lib/MainContent.svelte
@@ -0,0 +1,3 @@
+<main class="grid grid-cols-1 md:grid-cols-4 align-top justify-left px-4 md:px-8 py-1.5 md:py-3.5 gap-x-8 gap-y-2 md:gap-y-3.5 mx-2 md:mx-auto h-screen md:h-auto overflow-y-scroll snap-y snap-mandatory">
+    <slot />
+</main>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,64 +1,47 @@
 <script>
-	import Figure from '$lib/figure/Figure.svelte';
+    import Figure from '$lib/figure/Figure.svelte';
+    import Card from '$lib/cards/Card.svelte';
 	import { base } from '$app/paths';
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="P33" />
+    <title>Home</title>
+    <meta name="description" content="P33"/>
 </svelte:head>
 
-<div class="container flex space-x-4 overflow-scroll snap-x snap-mandatory">
-	<div class="space-y-4 text-center">
-		<div class="drop-shadow-card">
-			<h1 class="capitalize"><a href={`${base}/k8/access`}>K-8</a></h1>
-			<p>City of Chicago</p>
-		</div>
-		<div class="drop-shadow-card">
-			<Figure id="figure" />
-		</div>
-	</div>
-	<div class="space-y-4 text-center">
-		<div class="drop-shadow-card">
-			<h1 class="capitalize"><a href={`${base}/hs/access`}>High school</a></h1>
-			<p>City of Chicago</p>
-		</div>
-		<div class="drop-shadow-card">
-			<Figure id="figure" />
-		</div>
-	</div>
-	<div class="space-y-4 text-center">
-		<div class="drop-shadow-card">
-			<h1 class="capitalize"><a href={`${base}/college/access`}>College</a></h1>
-			<p>City of Chicago</p>
-		</div>
-		<div class="drop-shadow-card">
-			<Figure id="figure" />
-		</div>
-	</div>
-	<div class="space-y-4 text-center">
-		<div class="drop-shadow-card">
-			<h1 class="capitalize"><a href={`${base}/career/access`}>Career</a></h1>
-			<p>City of Chicago</p>
-		</div>
-		<div class="drop-shadow-card">
-			<Figure id="figure" />
-		</div>
-	</div>
+<div class="snap-start space-y-4 text-center h-screen">
+    <Card>
+		<h1 class="capitalize"><a href={`${base}/k8/access`}>K-8</a></h1>
+        <p>City of Chicago</p>
+    </Card>
+    <Card>
+        <Figure id="figure"/>
+    </Card>
 </div>
-
-<style>
-	.container > * {
-		min-width: calc(320px - 1rem);
-		width: calc(320px - 1rem);
-		scroll-snap-align: center;
-	}
-	.container > * > * {
-		background-color: white;
-		box-sizing: border-box;
-		padding: 1rem;
-	}
-	.container > * > * > :global(*) {
-		/* margin: 1rem; */
-	}
-</style>
+<div class="snap-start space-y-4 text-center h-screen">
+    <Card>
+		<h1 class="capitalize"><a href={`${base}/hs/access`}>High school</a></h1>
+        <p>City of Chicago</p>
+    </Card>
+    <Card>
+        <Figure id="figure"/>
+    </Card>
+</div>
+<div class="snap-start space-y-4 text-center h-screen">
+    <Card>
+		<h1 class="capitalize"><a href={`${base}/college/access`}>College</a></h1>
+        <p>City of Chicago</p>
+    </Card>
+    <Card>
+        <Figure id="figure"/>
+    </Card>
+</div>
+<div class="snap-start space-y-4 text-center h-screen">
+    <Card>
+		<h1 class="capitalize"><a href={`${base}/career/access`}>Career</a></h1>
+        <p>City of Chicago</p>
+    </Card>
+    <Card>
+        <Figure id="figure"/>
+    </Card>
+</div>
