0557dd38d9d89c9222f58f22bba58f5f46176103
j0vanka <jovanka.placeres@thoughtworks.com>
2022-09-14 23:28:40 +0000

    fix build

--- web/src/routes/(data)/+layout.svelte
+++ web/src/routes/(data)/+layout.svelte
@@ -4,10 +4,10 @@
 
 <a href={`${base}/`} class="leading-8 hidden md:grid col-span-1">&larr; Back</a>
 <div class="hidden md:block col-span-3 2xl:w-screen">
-    <a class="inline-block leading-8 bg-white border-2 rounded-full md:px-3 lg:px-6 uppercase" href="access">Access</a>
-    <a class="inline-block leading-8 bg-white border-2 rounded-full md:px-3 lg:px-6 uppercase" href="proficiency">Proficiency</a>
-    <a class="inline-block leading-8 bg-white border-2 rounded-full md:px-3 lg:px-6 uppercase" href="excellence">Excellence</a>
-    <a class="inline-block leading-8 bg-white border-2 rounded-full md:px-3 lg:px-6 uppercase" href="methodology">Methodology</a>
+    <a class="inline-block leading-8 bg-white border-2 rounded-full md:px-3 lg:px-6 uppercase" href="#">Access</a>
+    <a class="inline-block leading-8 bg-white border-2 rounded-full md:px-3 lg:px-6 uppercase" href="#">Proficiency</a>
+    <a class="inline-block leading-8 bg-white border-2 rounded-full md:px-3 lg:px-6 uppercase" href="#">Excellence</a>
+    <a class="inline-block leading-8 bg-white border-2 rounded-full md:px-3 lg:px-6 uppercase" href="#">Methodology</a>
 </div>
 
 <slot />
