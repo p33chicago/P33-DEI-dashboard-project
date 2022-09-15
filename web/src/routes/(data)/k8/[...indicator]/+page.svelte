371a89ebcd384c53a38e76e5ee1fa5abeb3bddd6
j0vanka <jovanka.placeres@thoughtworks.com>
2022-09-15 21:45:39 +0000

    wip:access layout

--- web/src/routes/(data)/k8/[...indicator]/+page.svelte
+++ web/src/routes/(data)/k8/[...indicator]/+page.svelte
@@ -3,18 +3,29 @@
     import Card from "$lib/cards/Card.svelte";
     import Lipsum from '$lib/Lipsum.svelte';
     import { base } from '$app/paths';
+    import Access from "$lib/Access.svelte"
 
     export let data;
 </script>
 
 <Card>
     <a href={`${base}/k8${ data.indicator ? '/' : '/access' }`} class="text-dark-gray">
-        <h1>Access</h1>
-        <p>This is the awesome stuff that some poeple have and some people don't</p>
+        <div class="grid grid-cols-2">
+            <div>
+                <h1>Access</h1>
+                <p>85.5</p>
+            </div>
+            <div class="pt-4">
+                <p>How much access do black and latino communities have to the resources 
+                and opportunities necessary to succeed on a computer science pathway?</p>
+            </div>
+        </div>
+        
         {#if data.indicator === 'access' }
-            <p transition:slide>
+            <!-- <p transition:slide>
                 <Lipsum words="30" />
-            </p>
+            </p> -->
+            <Access />
         {/if}
     </a>
 </Card>
