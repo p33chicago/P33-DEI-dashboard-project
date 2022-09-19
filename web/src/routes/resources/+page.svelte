e35954c42cd6da345d519bb7c8dcad458ab5aec3
j0vanka <jovanka.placeres@thoughtworks.com>
2022-09-19 20:33:00 +0000

    update resource page/placeholder layout

--- web/src/routes/resources/+page.svelte
+++ web/src/routes/resources/+page.svelte
@@ -4,25 +4,34 @@
 
 <CardPage>
     <h1>Resources</h1>
-    <div>
-        <p>
-            Pariatur tacos messenger bag squid post-ironic ut quis you probably haven't heard of them. 
-            Vinyl laboris umami plaid vaporware elit 90's gatekeep yes plz hoodie letterpress trust fund proident. 
-            Enim cillum migas brunch. Schlitz ramps ethical neutra.
-        </p>
-        <p>
-            Commodo cold-pressed pok pok pariatur consectetur sed. Magna fit mustache, 
-            sus consectetur banh mi vibecession ullamco eu chia lo-fi artisan pug laborum man bun. 
-            Wayfarers normcore green juice literally, tumblr hella nulla mumblecore meh trust fund. 
-            Taxidermy af sus, green juice same bruh single-origin coffee ugh everyday carry gochujang vibecession adipisicing scenester bespoke. 
-            Chambray +1 sus heirloom, irony pork belly salvia enim knausgaard seitan bruh duis tbh artisan wayfarers. 
-            Banh mi lo-fi mlkshk, cold-pressed direct trade kickstarter biodiesel proident sus reprehenderit in selfies tousled ullamco pug.
-        </p>
-        <p>
-            Cillum cred fugiat cloud bread waistcoat pickled ad. 
-            Kale chips food truck est, unicorn art party try-hard sustainable quis seitan et blog jianbing snackwave ut. 
-            Et schlitz brunch chillwave. Normcore everyday carry ut fanny pack. Church-key laboris lyft fixie aliqua. 
-            Literally crucifix street art tacos succulents sint
-        </p>
+    <h2 class="my-4">K-8</h2>
+    <div class="grid grid-cols-2 auto-rows-auto gap-x-16 mb-10">
+        <div class="mb-6">
+            <h3>Name of resource</h3>
+            <p>
+                Pariatur tacos messenger bag squid post-ironic ut quis you probably haven't heard of them. 
+                Vinyl laboris umami plaid vaporware elit 90's gatekeep yes plz hoodie letterpress trust fund proident. 
+                Enim cillum migas brunch. Schlitz ramps ethical neutra.
+            </p>
+        </div>  
+        
+        <div>
+            <h3>Name of resource</h3>
+            <p>
+                Pariatur tacos messenger bag squid post-ironic ut quis you probably haven't heard of them. 
+                Vinyl laboris umami plaid vaporware elit 90's gatekeep yes plz hoodie letterpress trust fund proident. 
+                Enim cillum migas brunch. Schlitz ramps ethical neutra.
+            </p>
+        </div>
+       
+        <div>
+            <h3>Name of resource</h3>
+            <p>
+                Pariatur tacos messenger bag squid post-ironic ut quis you probably haven't heard of them. 
+                Vinyl laboris umami plaid vaporware elit 90's gatekeep yes plz hoodie letterpress trust fund proident. 
+                Enim cillum migas brunch. Schlitz ramps ethical neutra.
+            </p>
+        </div>
+        
     </div>
 </CardPage>
