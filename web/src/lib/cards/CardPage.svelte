12ad0def7de61d7fc568e9d540b6f91f4fb4c340
j0vanka <jovanka.placeres@thoughtworks.com>
2022-09-21 20:37:09 +0000

    add padding to main nav pages

--- web/src/lib/cards/CardPage.svelte
+++ web/src/lib/cards/CardPage.svelte
@@ -1,5 +1,5 @@
-<div class="md:col-start-2 col-span-3">
-	<div class="block px-4 lg:px-6 py-4 shadow-lg bg-white">
+<div class="grid grid-cols-1 px-4 md:grid-cols-4 md:px-24 py-10 lg:px-44">
+	<div class="md:col-start-1 col-span-3 block px-4 lg:px-6 py-4 shadow-lg bg-white">
         <slot></slot>
 	</div>
 </div>
