85550c0016485d77e5d0b0deeb2c9afb0b8f340f
John Syrinek <john.syrinek@gmail.com>
2022-09-21 10:06:01 -0500

    Hides mobile nav after clickiong

--- web/src/lib/header/Header.svelte
+++ web/src/lib/header/Header.svelte
@@ -2,11 +2,14 @@
 	import { page } from '$app/stores';
 	import { assets, base } from '$app/paths'
 	import {default_scorecard_path, pathname_pattern} from "$lib/pathnames.js";
+
+	let nav_checkbox;
+	const hide_nav = () => nav_checkbox.checked = false;
 </script>
 
 <header class="font-mono w-full items-center fullnav:border-b border-brand-primary-green z-10">
 	<div class="flex justify-between fullnav:justify-start mx-auto max-w-7xl">
-		<input type="checkbox" id="open-mobile-menu" class="hidden" />
+		<input type="checkbox" id="open-mobile-menu" class="hidden" bind:this={nav_checkbox} />
 		<label for="open-mobile-menu" class="flex fullnav:hidden toggle-mobile-menu items-center justify-center p-2" aria-controls="mobile-menu" aria-expanded="false">
 			<span class="sr-only">Open main menu</span>
 			<svg class="hamburger block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
@@ -16,7 +19,7 @@
 				<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
 			</svg>
 		</label>
-		<div class="logo flex p-2">
+		<div class="logo flex p-2" on:click={hide_nav}>
 			<a href={`${base}/`} class="">
 				<img src={`${assets}/p33-logo.png`} class="h-8 w-8" />
 			</a>
@@ -25,7 +28,7 @@
 		<div class="flex fullnav:hidden h-10 w-10"><div> </div></div>
 
 		<nav class="hidden fullnav:flex w-full fullnav:w-auto absolute fullnav:static top-12 left-0 right-0 bottom-0 bg-white fullnav:bg-transparent p-4 fullnav:p-0 leading-10 border-t fullnav:border-0 border-brand-primary-green">
-			<ul class="fullnav:flex">
+			<ul class="fullnav:flex" on:click={hide_nav}>
 				<li class:active={pathname_pattern.test($page.url.pathname)}><a href={`${base}/${default_scorecard_path}`}>Scorecard</a></li>
 				<li class:active={$page.url.pathname === `${base}/methodology`}><a href={`${base}/methodology`}>Methodology</a></li>
 				<li class:active={$page.url.pathname === `${base}/resources`}><a href={`${base}/resources`}>Resources</a></li>
