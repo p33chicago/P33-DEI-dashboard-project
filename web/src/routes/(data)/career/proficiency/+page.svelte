14a0a510937d9c4a1566aa27367ab8c75410d0dd
jovanka <jovanka.placeres@thoughtworks.com>
2022-09-27 00:01:45 -0500

    add career copy

--- web/src/routes/(data)/career/proficiency/+page.svelte
+++ web/src/routes/(data)/career/proficiency/+page.svelte
@@ -1,1 +1,28 @@
-Proficiency content
+<script type="ts">
+    // import Figure from "$lib/figure/Figure.svelte";
+    import DataDive from "$lib/DataDive.svelte";
+    // import {career} from "$lib/domain/Lifestage.ts";
+</script>
+
+<DataDive
+        header="Chicago MSA tech employee demographics"
+        subheader="Proportion of Chicago MSA degree holders working in a top 11 tech occupation"
+>
+    <svelte:fragment slot="story">
+        <p>
+            In the Chicago metropolitan region,
+            the number of tech jobs occupied by Asian workers is 10% of the total number of Asian college graduates.
+            The ratio is 4% for white and Hispanic graduates and 2.5% for black graduates.
+        </p>
+        <p>
+            The ratio of tech job occupations to the number of college graduates in the Metropolitan region of the City of Chicago is slightly lower than the national average across all ethnic groups.
+        </p>
+    </svelte:fragment>
+
+    <svelte:fragment slot="takeaways">
+        <p>
+            Hispanic students have to compete with 100 peer graduates for 4 job opportunities in tech companies, Black students have to compete with 100 peer graduates for 2 job opportunities in tech companies.
+        </p>
+    </svelte:fragment>
+    <!--    <Figure lifestage={career.route} name="" slot="figure"/>-->
+</DataDive>
