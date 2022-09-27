e9c4a830a03c77d5441549d812a6d78f511ff538
jovanka <jovanka.placeres@thoughtworks.com>
2022-09-26 23:32:33 -0500

    add hs copy

--- web/src/routes/(data)/hs/access/+page.svelte
+++ web/src/routes/(data)/hs/access/+page.svelte
@@ -1,49 +1,87 @@
-<div class="py-6">
-    <h2>CPS K-8 STEM Magnet School Enrollment</h2>
-    <p>The proportion of CPS K-8 students...</p>
-</div>
-<div class="grid grid-cols-2 gap-4">
-    <div>
-        <h3>What does the data tell?</h3>
-        <p>Undead malus De horrifying poenae caro zombie,
-            implent sit ab Summus Craven Zombie undead, viscera Terror grusome max cerebro.
-            Cerebro mortuum ante? Nigh accedens, corpses infecti monstra walking ferae, A fames tremor.
-            Lumbering nostra viral mortuis Putridi for mauris sicut carne mortui creaturas in malum,
-            fascinati iam darkness burial agite shaun kingdead ferae monstra comedat.
+<script type="ts">
+    // import Figure from "$lib/figure/Figure.svelte";
+    import DataDive from "$lib/DataDive.svelte";
+    // import {hs} from "$lib/domain/Lifestage.ts";
+</script>
+
+<DataDive
+        header="AP CS Enrollment"
+        subheader="The proportion of CPS HS students enrolled in AP CS"
+>
+    <svelte:fragment slot="story">
+        <p>
+            Asian students have the highest AP CS enrollment rate of 8%. 1.2 times higher than White,
+            4 times higher than Hispanics, and 7 times higher than black.
         </p>
-    </div>
-    <div class="box-border h-96 w-128 p-4 border-4">GRAPH?</div>
-</div>
-<div class="py-6">
-    <a class="inline-block leading-8 bg-white border-2 rounded md:px-3 lg:px-4 uppercase" href="#">help fix
-        this</a>
-    <a class="inline-block leading-8 bg-white border-2 rounded md:px-3 lg:px-4 uppercase" href="#">download
-        data</a>
-    <a class="inline-block leading-8 bg-white border-2 rounded md:px-3 lg:px-4 uppercase" href="#">share
-        this</a>
-</div>
-<hr/>
-<div class="py-6">
-    <h2>8th Grade Enrollment</h2>
-    <p>The proportion of CPS 8th grade students...</p>
-</div>
-<div class="grid grid-cols-2 gap-4 p">
-    <div>
-        <h3>What does the data tell?</h3>
-        <p>Undead malus De horrifying poenae caro zombie,
-            implent sit ab Summus Craven Zombie undead, viscera Terror grusome max cerebro.
-            Cerebro mortuum ante? Nigh accedens, corpses infecti monstra walking ferae, A fames tremor.
-            Lumbering nostra viral mortuis Putridi for mauris sicut carne mortui creaturas in malum,
-            fascinati iam darkness burial agite shaun kingdead ferae monstra comedat.
+        <p>
+            AP CS enrollment rate of students in CPS is significantly higher than the national average.
+            The proportion of White students enroll to AP CS is 6 times higher than the national average.
         </p>
-    </div>
-    <div class="box-border h-96 w-128 p-4 border-4">GRAPH?</div>
-</div>
-<div class="py-6">
-    <a class="inline-block leading-8 bg-white border-2 rounded md:px-3 lg:px-4 uppercase" href="#">help fix
-        this</a>
-    <a class="inline-block leading-8 bg-white border-2 rounded md:px-3 lg:px-4 uppercase" href="#">download
-        data</a>
-    <a class="inline-block leading-8 bg-white border-2 rounded md:px-3 lg:px-4 uppercase" href="#">share
-        this</a>
-</div>
+    </svelte:fragment>
+    <svelte:fragment slot="takeaways">
+        <p>
+            CPS has an overall much higher AP CS enrolment rate in comparison with the national average across all ethnic groups.
+            However, the disparity is enormous, White and Asian students have an enrollment rate 3-7 times higher than Black and Hispanic students.
+        </p>
+    </svelte:fragment>
+<!--    <Figure lifestage={hs.route} name="" slot="figure"/>-->
+</DataDive>
+
+<DataDive
+        header="CPS HS STEM Magnet School Enrollment"
+        subheader="The proportion of CPS HS students enrolled in a STEM magnet school by race/ethnicity"
+>
+    <svelte:fragment slot="story">
+        <p>
+            Asian students have the highest AP CS enrollment rate of 8%. 1.2 times higher than White,
+            4 times higher than Hispanics, and 7 times higher than black.
+        </p>
+    </svelte:fragment>
+    <svelte:fragment slot="takeaways">
+        <p>
+            13.25% of Black students enrolled in STEM Magnet elementary schools while only 8.82% enrolled in STEM Magnet high school.
+        </p>
+        <p>
+            7.87% of Hispanic students enrolled in STEM Magnet elementary schools while 19.66% enrolled in STEM Magnet high school.
+        </p>
+    </svelte:fragment>
+    <!--    <Figure lifestage={hs.route} name="" slot="figure"/>-->
+</DataDive>
+
+<DataDive
+        header="CS Interest"
+        subheader="The proportion of CPS HS students indicating interest in an CS/IT career"
+>
+    <svelte:fragment slot="takeaways">
+        <p>
+            1 out of 4 Asian students express an interest in Computer Science,
+            in comparison with 1 out of 5 white students,
+            1 out of 10 Hispanic students,
+            and 1 out of 20 black students expressing the same interest.
+        </p>
+    </svelte:fragment>
+    <!--    <Figure lifestage={hs.route} name="" slot="figure"/>-->
+</DataDive>
+
+<DataDive
+        header="Advanced Math Enrollment"
+        subheader="The proportion of CPS SAT takers exceeding standards by race/ethnicity"
+>
+    <svelte:fragment slot="story">
+        <p>
+            In Chicago, almost 4 out of 10 White and Asian students in CPS enrolled in advanced math class,
+            3 out of 10 Black and Hispanic students enrolled in advanced math class.
+        </p>
+        <p>
+            Overall, the proportion of students who enrolled in advanced math classes in CPS is twice the proportion in the national average.
+        </p>
+    </svelte:fragment>
+
+    <svelte:fragment slot="takeaways">
+        <p>
+            Minor racial disparity detected in advanced math enrollment rate.
+            Superior performances of CPS in related to the national average.
+        </p>
+    </svelte:fragment>
+    <!--    <Figure lifestage={hs.route} name="" slot="figure"/>-->
+</DataDive>
