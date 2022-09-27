1d818ed564dae77ed1e1ddc37889f756e0eefad6
jovanka <jovanka.placeres@thoughtworks.com>
2022-09-27 00:15:24 -0500

    update k8 copy; excellence + proficiency

--- web/src/routes/(data)/k8/excellence/+page.svelte
+++ web/src/routes/(data)/k8/excellence/+page.svelte
@@ -1,21 +1,47 @@
-<script>
-	import DataDive from "$lib/DataDive.svelte";
+<script type="ts">
+    import Figure from "$lib/figure/Figure.svelte";
+    import DataDive from "$lib/DataDive.svelte";
+    import {k8} from "$lib/domain/Lifestage.ts";
 </script>
 
 <DataDive
-    sectionHeader="4th grade Math Advanced"
-    sectionSubHeader="The proportion of CPS 4th graders advanced in math by race/ethnicity"
-    talkingPoint1="In CPS 4th grade, Asian have the highest proportion of students who excel at math. The proportion is 1.6 times higher than White, 8 times higher than Hispanics, and 16 times higher than black."
-    talkingPoint2="The proportion of 4th graders who excelled at math is slightly higher in CPS than the national average across all ethnic groups. "
-    takeaway1="The gap/disparity in ethnic groups is significantly enlarged from proficient students to advanced students."
-    takeaway2=""
-/>
+        header="4th grade Math Advanced"
+        subheader="The proportion of CPS 4th graders advanced in math by race/ethnicity"
+>
+    <svelte:fragment slot="story">
+        <p>
+            In CPS 4th grade, Asian have the highest proportion of students who excel at math. The proportion is 1.6 times higher than White, 8 times higher than Hispanics, and 16 times higher than black.
+        </p>
+        <p>
+            The proportion of 4th graders who excelled at math is slightly higher in CPS than the national average across all ethnic groups.
+        </p>
+    </svelte:fragment>
+    <svelte:fragment slot="takeaways">
+        <p>
+            The gap/disparity in ethnic groups is significantly enlarged from proficient students to advanced students.
+        </p>
+    </svelte:fragment>
+<!--    <Figure lifestage={k8.route} name="noInt" slot="figure"/>-->
+</DataDive>
+
 <DataDive
-    sectionHeader="8th grade Math Advanced "
-    sectionSubHeader="The proportion of CPS 8th graders advanced in math by race/ethnicity"
-    talkingPoint1="In CPS 8th grade, Asian have the highest proportion of students who excel at math. The proportion is 1.7 times higher than White, 6 times higher than Hispanics, and 19 times higher than black."
-    talkingPoint2="The proportion of 8th graders who excelled at math is slightly higher in CPS than the national average across all ethnic groups."
-    takeaway1="The proportion of Hispanic students who excelled at math has increased by 50% from 4th to 8th grade. This increment in population is 2.8 times greater than the increment of the other three ethnic groups combined."
-    takeaway2=""
-/>
+        header="8th grade Math Advanced"
+        subheader="The proportion of CPS 8th graders advanced in math by race/ethnicity"
+>
+    <svelte:fragment slot="story">
+        <p>
+            In CPS 8th grade, Asian have the highest proportion of students who excel at math. The proportion is 1.7 times higher than White, 6 times higher than Hispanics, and 19 times higher than black.
+        </p>
+        <p>
+            The proportion of 8th graders who excelled at math is slightly higher in CPS than the national average across all ethnic groups.
+        </p>
+    </svelte:fragment>
+    <svelte:fragment slot="takeaways">
+        <p>
+            The proportion of Hispanic students who excelled at math has increased by 50% from 4th to 8th grade. This increment in population is 2.8 times greater than the increment of the other three ethnic groups combined.
+        </p>
+    </svelte:fragment>
+    <!--    <Figure lifestage={k8.route} name="noInt" slot="figure"/>-->
+</DataDive>
+
 
