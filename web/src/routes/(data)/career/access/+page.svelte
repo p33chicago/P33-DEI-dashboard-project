551b17ce100097a3cbfdb954e8b2186df1a4a0e1
John Syrinek <john.syrinek@gmail.com>
2022-09-28 10:26:40 -0500

    cleanup

--- web/src/routes/(data)/career/access/+page.svelte
+++ web/src/routes/(data)/career/access/+page.svelte
@@ -1,9 +1,3 @@
-<script type="ts">
-    // import Figure from "$lib/figure/Figure.svelte";
-    import DataDive from "$lib/DataDive.svelte";
-    // import {career} from "$lib/domain/Lifestage.ts";
-</script>
-
 <div>
     <p>
         This component has no underlying metrics and is not included in calculation of the Equity Score.
