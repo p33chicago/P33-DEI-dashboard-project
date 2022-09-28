6cbdf0b931fca1a309423b2b8131ca49aa29db04
John Syrinek <john.syrinek@gmail.com>
2022-09-28 15:55:35 -0500

    Better output for build.py

--- build.py
+++ build.py
@@ -1,6 +1,13 @@
 from p33py.output import output_dir, figures, tables
 
 output_dir('./output')
+print('Writing figure JSON')
 figures(format='json')
-tables()
+print()
+
+print('Writing figure SVGs')
 figures(format='svg')
+print()
+
+print('Writing tables')
+tables()
