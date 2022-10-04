c0f0d5b0537562c84384978fedf18656d142bec5
jovanka <jovanka.placeres@thoughtworks.com>
2022-10-04 15:18:19 -0500

    update methodology page

--- web/src/lib/cards/CardPage.svelte
+++ web/src/lib/cards/CardPage.svelte
@@ -1,5 +1,5 @@
 <div class="grid grid-cols-1 px-4 md:grid-cols-4 md:px-24 py-10 lg:px-44">
-	<div class="md:col-start-1 col-span-3 block px-4 lg:px-6 py-4 shadow-lg bg-white">
+	<div class="md:col-start-1 col-span-3 block p-4 lg:p-10 shadow-lg bg-white">
         <slot></slot>
 	</div>
 </div>
--- web/src/routes/methodology/+page.svelte
+++ web/src/routes/methodology/+page.svelte
@@ -4,13 +4,20 @@
 </script>
 
 <CardPage>
-    <h1>Methodology</h1>
+    <h1 class="pb-2">Methodology</h1>
+    <hr class="border-1 border-light-gray"/>
     <div class="col-span-1 md:col-span-4">
-        <p>All data is based on a Index of Disparity constructed in a research paper (<a target="_blank"
-                                                                                        href="https://open.umich.edu/sites/default/files/downloads/PublicHealthRep-Pearcy.pdf">Percy
-            Keppel 2002</a>) and adopted by Policy
-            Link’s <a target="_blank" href="https://nationalequityatlas.org/about-the-atlas/methodology/indexmethod">National
-                Equity Index</a>.</p>
+        <p class="bg-really-light-gray my-8 pl-6 pr-2 py-4 font-mono">
+            The Equity Index scores are based on more than 20 individual metrics secured primarily from government and other official sources,
+            using the latest available data for each metric. We intend to add additional metrics, comparisons with individual cities, and data to show progress over time.
+        </p>
+        <p>All data is based on a Index of Disparity constructed in a research paper
+            (<a target="_blank"
+                href="https://open.umich.edu/sites/default/files/downloads/PublicHealthRep-Pearcy.pdf"
+                class="text-brand-primary-green">Percy Keppel 2002</a>) and adopted by Policy Link’s
+            <a target="_blank"
+                href="https://nationalequityatlas.org/about-the-atlas/methodology/indexmethod"
+                class="text-brand-primary-green">National Equity Index</a>.</p>
         <p>This equation is relatively simple, performs well at diagnosing inequity, and offers an unbiased approach when
             assessing different race/ethnic groups and population sizes.</p>
         <p>In short, the Index of Disparity measures the dispersion of a given subgroup (in our case race/ethnicity) around
@@ -32,8 +39,17 @@
         <p>Lastly, we find the life stage score (K-8, HS, College, Career) by taking the geometric mean of each framework
             score
             in each life stage.</p>
-        <p class="text-xl py-4"> <MathEquation math={`Index~of~disparity = \\frac{\\sum{[|r_i - R|]}}{R * N}`} /> </p>
-        <p class="text-xl py-4"> <MathEquation math={`Geometric Mean = \\sqrt[N]{x_1 * x_2 * ... * x_N}`} /> </p>
-        <p class="text-xl py-4"> <MathEquation math={`Scaling Equation = 100 - \\frac{Index~of~Disparity_i - Min(Index~of~Disparity)}{Max(Index~of~Disparity)}`} /> </p>
+        <div class="bg-really-light-gray mt-10">
+            <h2 class="uppercase p-4 text-center font-sans-alternate">index calculation</h2>
+            <p class="py-2 text-center font-sans-alternate"> <MathEquation math={`Index~of~disparity = \\frac{\\sum{[|r_i - R|]}}{R * N}`} /> </p>
+            <p class="py-2 text-center font-sans-alternate"> <MathEquation math={`Geometric Mean = \\sqrt[N]{x_1 * x_2 * ... * x_N}`} /> </p>
+            <p class="py-6 text-center font-sans-alternate"> <MathEquation math={`Scaling Equation = 100 - \\frac{Index~of~Disparity_i - Min(Index~of~Disparity)}{Max(Index~of~Disparity)}`} /> </p>
+            <hr class="border-1 border-light-gray mx-4"/>
+            <div class="p-6">
+                <p><span class="text-brand-primary-green">Proficiency:</span> How well are we preparing Black and Latino students to pursue and succeed on Computer Science pathways?</p>
+                <p><span class="text-brand-primary-green">Access:</span> How much access do Black and Latino communities have to the resources and opportunities necessary to succeed on Computer Science pathways?</p>
+                <p><span class="text-brand-primary-green">Excellence:</span> How well are we preparing Black and Latino students to succeed at elite levels on Computer Science pathways?</p>
+            </div>
+        </div>
     </div>
 </CardPage>
