c891eca82f594ab71e4873992f7bd7f82bf97857
John Syrinek <john.syrinek@gmail.com>
2022-10-04 14:18:42 -0500

    scorecard icons

--- web/src/lib/scorecard/lifestage_score/Header.svelte
+++ web/src/lib/scorecard/lifestage_score/Header.svelte
@@ -1,12 +1,23 @@
 <script type="ts">
     import type {Lifestage} from "$lib/domain/Lifestage";
+    import {assets} from "$app/paths";
 
     export let lifestage: Lifestage;
     export let area: string;
     export let score: number;
 </script>
-<div class="flex">
-    <div>img</div>
+<div class="flex items-start gap-2">
+    <div class="pt-1 h-[22px]">
+        {#if lifestage.name === 'K-8'}
+            <img src={`${assets}/spaceship.svg`}/>
+        {:else if lifestage.name === 'High School'}
+            <img src={`${assets}/certificate.svg`}/>
+        {:else if lifestage.name === 'College'}
+            <img src={`${assets}/mortarboard-cap.svg`}/>
+        {:else}
+            <img src={`${assets}/handshake.svg`}/>
+        {/if}
+    </div>
     <div class="grow">
         <h1 class="text-2xl text-black">{lifestage.name}</h1>
         <h2 class="text-sm uppercase font-sans-alternate text-dark-gray">{area}</h2>
