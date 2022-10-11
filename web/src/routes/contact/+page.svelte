529b2ee49b6f74b7bc35532a1e4c01e645b288c5
John Syrinek <john.syrinek@gmail.com>
2022-10-11 12:36:15 -0500

    styling for contact form

--- web/src/routes/contact/+page.svelte
+++ web/src/routes/contact/+page.svelte
@@ -10,7 +10,7 @@
 
 <BannerTitle title="Contact Us" />
 <BodyContentContainer>
-	<div class="col-span-1 sm:col-span-4 py-8">
+	<div class="contact-form text-black col-span-1 sm:col-span-4 py-8">
 		{#if browser}
 			<script>
 				if (typeof globalThis.hbspt === 'undefined') {
@@ -25,3 +25,46 @@
 		{/if}
 	</div>
 </BodyContentContainer>
+
+<style>
+	.contact-form :global(fieldset:not([hidden])) + :global(fieldset:not([hidden])) {
+		margin-top: theme('margin.8');
+	}
+
+	:global(.hs-form-required) {
+		color: theme('colors.fail-red');
+		padding-left: theme('padding.1');
+	}
+
+	:global(.hs-error-msgs) {
+		color: theme('colors.fail-red');
+	}
+
+	.contact-form :global(input:not([type='button']):not([type='submit'])),
+	.contact-form :global(textarea) {
+		background-color: theme('backgroundColor.really-light-gray');
+		border-radius: 0px;
+		border-bottom: 2px solid theme('colors.brand-primary-dark-green');
+		line-height: theme('lineHeight.loose');
+		padding-left: theme('padding.4');
+		padding-right: theme('padding.4');
+	}
+	.contact-form :global(input[type='button']),
+	.contact-form :global(input[type='submit']) {
+		/* inline-block leading-10 bg-brand-primary-dark-green text-white border-2 rounded-full font-sans-alternate mt-3.5 px-8 sm:px-11 uppercase */
+		line-height: theme('lineHeight.relaxed');
+		background-color: theme('backgroundColor.brand-primary-dark-green');
+		color: theme('textColor.white');
+		border-width: theme('borderWidth.2');
+		border-radius: theme('borderRadius.full');
+		font-family: theme('fontFamily.sans-alternate'), serif;
+		padding-left: theme('padding.11');
+		padding-right: theme('padding.11');
+		text-transform: uppercase;
+	}
+
+	.contact-form :global(input[type='radio']),
+	.contact-form :global(input[type='checkbox']) {
+		margin-right: theme('margin.2');
+	}
+</style>
