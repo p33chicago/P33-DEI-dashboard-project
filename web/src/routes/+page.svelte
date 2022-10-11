a2917dfbbbde7bb915c6384ec7dbcf5f7a225cd7
mmmuench <112517884+mmmuench@users.noreply.github.com>
2022-10-11 16:44:21 -0500

    Update +page.svelte

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -16,7 +16,7 @@
 
 <svelte:head>
 	<title>Tech Equity Index</title>
-	<meta content="Help promote equity in tech." name="description" />
+	<meta content="Chicago's collaborative effort to build the most inclusive and equitable tech city in the country." name="description" />
 </svelte:head>
 
 <!-- HERO -->
