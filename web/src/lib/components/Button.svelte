3549a405c8be25c759097519ad0d990806c4b167
John Syrinek <john.syrinek@gmail.com>
2022-10-13 11:13:41 -0500

    creates button component

---
+++ web/src/lib/components/Button.svelte
@@ -0,0 +1,11 @@
+<script type="ts">
+	export let href: string;
+</script>
+
+<a
+	class="{$$props.class ||
+		''} inline-block leading-10 bg-brand-primary-dark-green text-white border-2 rounded-full font-sans-alternate px-8 sm:px-11 uppercase"
+	{href}
+>
+	<slot />
+</a>
--- web/src/lib/components/CTASection.svelte
+++ web/src/lib/components/CTASection.svelte
@@ -1,4 +1,6 @@
 <script type="ts">
+	import Button from '$lib/components/Button.svelte';
+
 	export let title: string;
 	export let href: string;
 	export let link_text: string;
@@ -8,10 +10,5 @@
 <div class="px-9 py-7">
 	<h2 class="text-3xl font-sans-alternate text-black">{title}</h2>
 	<slot />
-	<a
-		class="inline-block leading-10 bg-brand-primary-dark-green text-white border-2 rounded-full font-sans-alternate mt-3.5 px-8 sm:px-11 uppercase"
-		{href}
-	>
-		{link_text}
-	</a>
+	<Button class="mt-3.5" {href}>{link_text}</Button>
 </div>
