52754cacd892f43c332f803881f92091ff4f8c6a
John Syrinek <john.syrinek@gmail.com>
2022-10-13 15:36:36 -0500

    allows classes to be used on <BodyContentContainer />

--- web/src/lib/components/BodyContentContainer.svelte
+++ web/src/lib/components/BodyContentContainer.svelte
@@ -1,5 +1,6 @@
 <div
-	class="w-full max-w-7xl grid grid-cols-1 md:grid-cols-4 align-top justify-left gap-x-6 gap-y-12 md:gap-y-3.5 px-7 lg:px-8 md:mx-auto"
+	class="{$$props.class ||
+		''} w-full max-w-7xl grid grid-cols-1 md:grid-cols-4 align-top justify-left gap-x-6 gap-y-12 md:gap-y-3.5 px-7 lg:px-8 md:mx-auto"
 >
 	<slot />
 </div>
