b241c53564e75db9ec6c0ca6e20111fab3760704
John Syrinek <john.syrinek@gmail.com>
2022-10-13 21:24:26 -0500

    accordion for data indicator sections

--- web/src/lib/components/IndicatorContent.svelte
+++ web/src/lib/components/IndicatorContent.svelte
@@ -7,24 +7,34 @@
 	import Caret from '$lib/components/icons/Caret.svelte';
 
 	export let active_indicator: Indicator;
+
+	$: is_active = (indicator: Indicator) => active_indicator.name === indicator.name;
 </script>
 
 {#each indicators as indicator}
-	<ConditionalLink
-		active={active_indicator.name !== indicator.name}
-		href={indicator.route}
-		class="md:hidden"
-	>
+	<ConditionalLink active={!is_active(indicator)} href={indicator.route} class="md:hidden">
 		<Card>
-			<Heading nohr={active_indicator.name !== indicator.name} class="flex">
-				<span class="grow"> {indicator.name}</span>
-				<span class="self-center" class:rotate-180={active_indicator.name === indicator.name}>
-					<Caret />
-				</span>
-			</Heading>
-			{#if active_indicator.name === indicator.name}
-				<slot />
-			{/if}
-		</Card>
+			<details open={is_active(indicator)} class:active={is_active(indicator)}>
+				<summary class="list-none">
+					<Heading nohr class="flex">
+						<span class="grow"> {indicator.name}</span>
+						<span class="caret self-center sm:hidden">
+							<Caret />
+						</span>
+					</Heading>
+				</summary>
+				{#if is_active(indicator)}
+					<div class="border-light-gray border-t-[1px]">
+						<slot />
+					</div>
+				{/if}
+			</details></Card
+		>
 	</ConditionalLink>
 {/each}
+
+<style>
+	details[open] .caret {
+		transform: rotate(180deg);
+	}
+</style>
